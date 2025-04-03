cf53c1eb9318768ef871373e657339d5b41b1089
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-04-03 17:37:12 +0000

    Add Zeek logs to ontology output

--- zeek/zeek.py
+++ zeek/zeek.py
@@ -1,3 +1,5 @@
+"""This Assemblyline service uses Zeek to analyze PCAP files."""
+
 import json
 import os
 import re
@@ -8,20 +10,23 @@ from assemblyline.odm.base import IP_ONLY_REGEX
 from assemblyline.odm.models.ontology.results.network import NetworkConnection
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.request import ServiceRequest
-from assemblyline_v4_service.common.result import Result, ResultTableSection, TableRow
+from assemblyline_v4_service.common.result import Result, ResultSection, ResultTableSection, TableRow
 
 IP_REGEX = re.compile(IP_ONLY_REGEX)
 
 
 class DedupResultTableSection(ResultTableSection):
+    """This class is used to generate a TableSection with de-duplicated rows."""
+
     def add_row(self, row):
+        """Add unique rows to the table."""
         # Minimize on duplicate rows
         if row not in self.section_body._data:
-            return super().add_row(row)
+            super().add_row(row)
 
 
 class Zeek(ServiceBase):
-    """This Assemblyline service uses Zeek to analyze PCAP files."""
+    """This Assemblyline service class uses Zeek to analyze PCAP files."""
 
     def execute(self, request: ServiceRequest):
         """Run the service."""
@@ -32,19 +37,50 @@ class Zeek(ServiceBase):
             ["/opt/zeek/bin/zeek", "-r", request.file_path, "LogAscii::use_json=T"], cwd=self.working_directory
         )
 
+        # Add all log files as supplementary files and include them in the ontology
         log_files = [file for file in os.listdir(self.working_directory) if file.endswith(".log")]
-        # TODO: Will add all log files as supplementary for now until we know what's important
         for log_file in log_files:
+            filepath = os.path.join(self.working_directory, log_file)
             request.add_supplementary(
-                path=os.path.join(self.working_directory, log_file),
+                path=filepath,
                 name=log_file,
                 description=f"{log_file[:-4].upper()} logs from Zeek",
             )
+            with open(filepath, "r") as log:
+                self.ontology.add_other_part(log_file, {"encoding": "raw", "tool": "zeek", "content": log.read()})
+
+        # Extracted files
+        if "files.log" in log_files:
+            file_extracted_section = ResultSection("File(s) extracted by Zeek", parent=result)
+            log_path = os.path.join(self.working_directory, "files.log")
+            added_files = []
+            # Add extracted files to result
+            with open(log_path) as f:
+                for log in f.read().splitlines():
+                    log = json.loads(log)
+
+                    filepath = os.path.join(self.working_directory, "extract_files", log["extracted"])
+                    with open(filepath, "rb") as extracted_file:
+                        file_sha256 = str(sha256(extracted_file.read()).digest())
+
+                    # Add file to result if it hasn't been added yet
+                    if file_sha256 not in added_files:
+                        added_files.append(file_sha256)
+                        filename = log["extracted"]
+                        request.add_extracted(
+                            path=filepath,
+                            name=filename,
+                            description=f"Extracted file from {log['source']}",
+                        )
+
+                        file_extracted_section.add_line(filename)
+                        file_extracted_section.add_tag("file.name.extracted", filename)
+                        file_extracted_section.add_tag("file.name.extracted", file_sha256)
 
         # HTTP
         if "http.log" in log_files:
             log_path = os.path.join(self.working_directory, "http.log")
-            http_section = DedupResultTableSection("HTTP Logs")
+            http_section = DedupResultTableSection("HTTP Logs", parent=result)
             with open(log_path) as f:
                 for log in f.read().splitlines():
                     log = json.loads(log)
@@ -97,11 +133,10 @@ class Zeek(ServiceBase):
                     if not log["host"].startswith(log["id.resp_h"]):
                         # Tag hostname if applicable
                         http_section.add_tag("network.static.domain", log["host"])
-            result.add_section(http_section)
 
         if "dns.log" in log_files:
             log_path = os.path.join(self.working_directory, "dns.log")
-            dns_section = DedupResultTableSection("DNS Logs")
+            dns_section = DedupResultTableSection("DNS Logs", parent=result)
             with open(log_path) as f:
                 for log in f.read().splitlines():
                     log = json.loads(log)
@@ -128,27 +163,5 @@ class Zeek(ServiceBase):
                             dns_section.add_tag("network.static.ip", answer)
                         else:
                             dns_section.add_tag("network.static.domain", answer)
-            result.add_section(dns_section)
-
-        if "files.log" in log_files:
-            log_path = os.path.join(self.working_directory, "files.log")
-            added_files = []
-            # Add extracted files to result
-            with open(log_path) as f:
-                for log in f.read().splitlines():
-                    log = json.loads(log)
-
-                    filepath = os.path.join(self.working_directory, "extract_files", log["extracted"])
-                    with open(filepath, "rb") as extracted_file:
-                        file_sha256 = str(sha256(extracted_file.read()).digest())
-
-                    # Add file to result if it hasn't been added yet
-                    if file_sha256 not in added_files:
-                        added_files.append(file_sha256)
-                        request.add_extracted(
-                            path=filepath,
-                            name=log["extracted"],
-                            description=f"Extracted file from {log['source']}",
-                        )
 
         request.result = result
